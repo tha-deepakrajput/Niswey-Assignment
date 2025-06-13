cd364075de7ceb3d8fdb4940b28415ef25e77710
tha-deepakrajput <deepak23@navgurukul.org>
2025-06-13 13:50:21 +0530

    task update and task delete route is created

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const server = http.createServer((req, res) => {
         return;
     }
 
-    if (method === "POST" && path === "/tasks") {
+    else if (method === "POST" && path === "/tasks") {
         let body = '';
 
         req.on("data", chunk => body += chunk);
@@ -43,6 +43,71 @@ const server = http.createServer((req, res) => {
         return;
     }
 
+    else if (method === "PUT" && path.startsWith("/tasks/")) {
+        const id = parseInt(path.split("/")[2]);
+
+        if (isNaN(id)) {
+            res.writeHead(400, { "content-type": "application/json" });
+            res.end(JSON.stringify({ error: "Invalid Id" }));
+            return;
+        }
+
+        let body = "";
+        req.on("data", chunk => body += chunk);
+        req.on("end", () => {
+            try {
+                const { title } = JSON.parse(body);
+
+                if (!title || typeof title !== "string") {
+                    res.writeHead(400, { "content-type": "application/json" });
+                    res.end(JSON.stringify({ error: "Title is required" }));
+                    return;
+                }
+
+                const task = tasks.find((task) => task.id === id);
+
+                if (!task) {
+                    res.writeHead(400, { "content-type": "application/json" });
+                    res.end(JSON.stringify({ error: "Task Not Found!" }));
+                    return;
+                }
+
+                task.title = title;
+
+                res.writeHead(200, { "content-type": "application/json" });
+                res.end(JSON.stringify(task));
+
+            } catch (err) {
+                res.writeHead(400, { "content-type": "application/json" });
+                res.end(JSON.stringify({ error: "Invalid JSON format" }));
+            }
+        });
+        return;
+    }
+
+    else if (method === "DELETE" && path.startsWith("/tasks/")) {
+
+        const id = parseInt(path.split('/')[2]);
+
+        const taskIndex = tasks.findIndex((task) => task.id === id);
+
+        if (taskIndex === -1) {
+            res.writeHead(400, { "content-type": "appliation/json" });
+            res.end(JSON.stringify({ error: "Task Not Found!" }));
+            return;
+        }
+
+        tasks.splice(taskIndex, 1);     // It will remove the task.  
+
+        res.writeHead(200, { "content-type": "application/json" });
+        res.end(JSON.stringify({ message: "Task Deleted Successfully" }));
+    }
+
+    else {
+        res.writeHead(400, { "content-type": "application/json" });
+        res.end(JSON.stringify({ error: "Route Not Found!" }));
+        return;
+    }
 });
 
 const PORT = 3000;
